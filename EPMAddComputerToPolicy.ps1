f396e67b2b6d356463781a5c8a616081fd0fafcf
Giulio Compagnone <giulio.compagnone@cyberark.com>
2025-05-30 17:24:14 +0200

    Add EPMAddComputerToPolicy.ps1

---
+++ EPMAddComputerToPolicy.ps1
@@ -0,0 +1,478 @@
+<#
+.SYNOPSIS
+    
+
+.DESCRIPTION
+
+.PARAMETER username
+    The EPM username (e.g., user@domain).
+
+.PARAMETER setName
+    The name of the EPM set.
+
+.PARAMETER tenant
+    The EPM tenant name (e.g., eu, uk).
+
+.PARAMETER destinationFolder
+
+
+.NOTES
+    File: EPMBaseFunc.ps1
+    Author: Giulio Compagnone
+    Company: CyberArk
+    Version: 2
+    Created: 05/2023
+    Last Modified: 03/2025
+#>
+
+param (
+    [Parameter(Mandatory = $true, HelpMessage="Please enter valid EPM username (For example: user@domain)")]
+    [string]$username,
+
+    [Parameter(HelpMessage="Please enter valid EPM set name")]
+    [string]$setName,
+
+    [Parameter(Mandatory = $true, HelpMessage="Please enter valid EPM tenant (eu, uk, ....)")]
+    [ValidateSet("login", "eu", "uk", "au", "ca", "in", "jp", "sg", "it", "ch")]
+    [string]$tenant,
+
+    [Parameter(HelpMessage = "Enable logging to file and console")]
+    [switch]$log,
+
+    [Parameter(HelpMessage = "Specify the log file path")]
+    [string]$logFolder,
+
+    [Parameter(Mandatory=$true)]
+    [string]$policyFile
+
+)
+
+# Function to log messages to console and file
+function Write-Log {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$message,
+        
+        [Parameter(Mandatory = $true)]
+        [ValidateSet("INFO", "WARN", "ERROR")]
+        [string]$severity,
+
+        [ValidateSet("Black", "DarkBlue", "DarkGreen", "DarkCyan", "DarkRed", "DarkMagenta", "DarkYellow", "Gray", "DarkGray", "Blue", "Green", "Cyan", "Red", "Magenta", "Yellow", "White")]
+        [string]$ForegroundColor
+    )
+    
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $logMessage = "$timestamp [$severity] - $message"
+
+    switch ($severity) {
+        "INFO" {
+            if (-not $PSBoundParameters.ContainsKey("ForegroundColor")) {
+                $ForegroundColor = "Green"
+            }
+            Write-Host $logMessage -ForegroundColor $ForegroundColor
+        }
+        "WARN" {
+            if (-not $PSBoundParameters.ContainsKey("ForegroundColor")) {
+                $ForegroundColor = "Yellow"
+            }
+            Write-Host $logMessage -ForegroundColor $ForegroundColor
+        }
+        "ERROR" {
+            if (-not $PSBoundParameters.ContainsKey("ForegroundColor")) {
+                $ForegroundColor = "Red"
+            }
+            Write-Host $logMessage -ForegroundColor $ForegroundColor
+        }
+    }
+
+    if ($log) {
+        Add-Content -Path $logFilePath -Value $logMessage
+    }
+}
+
+function Write-Box {
+    param (
+        [string]$title
+    )
+    
+    # Calculate the length of the title
+    $titleLength = $title.Length
+
+    # Create the top and bottom lines
+    $line = "-" * $titleLength
+
+    # Print the box
+    Write-Log "+ $line +" -severity INFO -ForegroundColor Cyan
+    Write-Log "| $title |" -severity INFO -ForegroundColor Cyan
+    Write-Log "+ $line +" -severity INFO -ForegroundColor Cyan
+}
+
+function Invoke-EPMRestMethod {
+    <#
+    .SYNOPSIS
+        Invokes a REST API method with automatic retry logic in case of transient failures.
+
+    .DESCRIPTION
+        This function is designed to make REST API calls with automatic retries in case of specific errors, such as rate limiting.
+        It provides a robust way to handle transient failures and ensures that the API call is retried a specified number of times.
+
+    .PARAMETER URI
+        The Uniform Resource Identifier (URI) for the REST API endpoint.
+
+    .PARAMETER Method
+        The HTTP method (e.g., GET, POST, PUT, DELETE) for the API call.
+
+    .PARAMETER Body
+        The request body data to be sent in the API call (can be null for certain methods).
+
+    .PARAMETER Headers
+        Headers to include in the API request.
+    #>
+    
+    param (
+        [string]$URI,
+        [string]$Method,
+        [object]$Body = $null,
+        [hashtable]$Headers = @{},
+        [int]$MaxRetries = 3,
+        [int]$RetryDelay = 120
+    )
+
+    $retryCount = 0
+
+    while ($retryCount -lt $MaxRetries) {
+        try {
+            # Write-Log "Attempt #$($retryCount + 1): Calling API: $URI with Method: $Method" INFO
+            $response = Invoke-RestMethod -Uri $Uri -Method $Method -Body $Body -Headers $Headers -ErrorAction Stop
+            return $response
+        }
+        catch {
+            #$errorMessage = $_.Exception.Message
+            $ErrorDetailsMessage = $null
+
+            # Extract API error details if available
+            if ($_.ErrorDetails -and $_.ErrorDetails.Message) {
+                try {
+                    $ErrorDetailsMessage = $_.ErrorDetails.Message | ConvertFrom-Json -ErrorAction SilentlyContinue
+                }
+                catch {
+                    Write-Log "Failed to parse error message as JSON. Raw message: $($_.ErrorDetails.Message)" WARN
+                }
+            }
+
+            # Handle rate limit error (EPM00000AE)
+            if ($ErrorDetailsMessage -and $ErrorDetailsMessage.ErrorCode -eq "EPM00000AE") {
+                Write-Log "$($ErrorDetailsMessage.ErrorMessage) - Retrying in $RetryDelay seconds..." WARN
+                Start-Sleep -Seconds $RetryDelay
+                $retryCount++
+            } else {
+                # Handle Body possible filter error 
+                if ($ErrorDetailsMessage.ErrorCode -eq "EPM000002E" -and $null -ne $Body) {
+                    Write-Log "API call failed at line $($MyInvocation.ScriptLineNumber) - ErrorCode: $($ErrorDetailsMessage.ErrorCode), ErrorMessage: $($ErrorDetailsMessage.ErrorMessage)" ERROR
+                    Write-Log "Please verify the filter body if present, as it could be the cause of this error code." ERROR
+                    throw "API call failed at line $($MyInvocation.ScriptLineNumber) - ErrorCode: $($ErrorDetailsMessage.ErrorCode), ErrorMessage: $($ErrorDetailsMessage.ErrorMessage)"
+                } else {
+                    # Log error only if it's NOT the handled EPM00000AE error
+                    Write-Log "API call failed at line $($MyInvocation.ScriptLineNumber) - ErrorCode: $($ErrorDetailsMessage.ErrorCode), ErrorMessage: $($ErrorDetailsMessage.ErrorMessage)" ERROR
+                    throw "API call failed at line $($MyInvocation.ScriptLineNumber) - ErrorCode: $($ErrorDetailsMessage.ErrorCode), ErrorMessage: $($ErrorDetailsMessage.ErrorMessage)"
+                }
+            }
+        }
+    }
+
+    # If all retries fail, log and throw an error
+    Write-Log "API call failed after $MaxRetries retries. URI: $URI" ERROR
+    throw "API call failed after $MaxRetries retries."
+}
+
+function Connect-EPM {
+    <#
+    .SYNOPSIS
+    Connects to the EPM (Enterprise Password Vault) using the provided credentials and tenant information.
+
+    .DESCRIPTION
+    This function performs authentication with the EPM API to obtain the manager URL and authentication details.
+
+    .PARAMETER credential
+    The credential object containing the username and password.
+
+    .PARAMETER epmTenant
+    The EPM tenant name.
+
+    .OUTPUTS
+    A custom object with the properties "managerURL" and "auth" representing the EPM connection information.
+
+    #>
+    param (
+        [Parameter(Mandatory = $true)]
+        [pscredential]$credential,  # Credential object containing the username and password
+
+        [Parameter(Mandatory = $true)]
+        [string]$epmTenant          # EPM tenant name
+    )
+
+    # Convert credential information to JSON for authentication
+    $authBody = @{
+        Username      = $credential.UserName
+        Password      = $credential.GetNetworkCredential().Password
+        ApplicationID = "Powershell"
+    } | ConvertTo-Json -Depth 3
+
+    $authHeaders = @{
+        "Content-Type" = "application/json"
+    }
+
+    try {
+        # Write-Log "Attempting to connect to EPM tenant: $epmTenant" INFO
+        $response = Invoke-EPMRestMethod -URI "https://$epmTenant.epm.cyberark.com/EPM/API/Auth/EPM/Logon" -Method 'POST' -Headers $authHeaders -Body $authBody
+
+        # Ensure the response contains the expected fields
+        if (-not $response -or -not $response.ManagerURL -or -not $response.EPMAuthenticationResult) {
+            throw "EPM authentication failed: Missing expected response fields."
+        }
+
+        # Write-Log "Successfully connected to EPM tenant: $epmTenant" INFO
+
+        # Return a custom object with connection information
+        return [PSCustomObject]@{
+            managerURL = $response.ManagerURL
+            auth       = $response.EPMAuthenticationResult
+        }
+    }
+    catch {
+        Write-Log "Failed to connect to EPM tenant: $epmTenant. Error: $_" ERROR
+        throw "Error connecting to EPM: $_"
+    }
+}
+
+function Get-EPMSetID {
+    <#
+    .SYNOPSIS
+    Retrieves the ID and name of an EPM set based on the provided parameters.
+
+    .DESCRIPTION
+    This function interacts with the EPM API to retrieve information about sets based on the specified parameters.
+
+    .PARAMETER managerURL
+    The URL of the EPM manager.
+
+    .PARAMETER Headers
+    The authorization headers.
+
+    .PARAMETER setName
+    The name of the EPM set to retrieve.
+
+    .OUTPUTS
+    A custom object with the properties "setId" and "setName" representing the EPM set information.
+    #>
+
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$managerURL,
+
+        [Parameter(Mandatory = $true)]
+        [hashtable]$Headers,
+
+        [string]$setName
+    )
+
+    # Retrieve list of sets
+    try {
+        #Write-Log "Retrieving EPM Sets from: $managerURL" INFO
+        $sets = Invoke-EPMRestMethod -URI "$managerURL/EPM/API/Sets" -Method 'GET' -Headers $Headers
+
+        if (-not $sets -or -not $sets.Sets) {
+            throw "No sets retrieved from EPM."
+        }
+    }
+    catch {
+        Write-Log "Failed to retrieve EPM Sets. Error: $_" ERROR
+        throw "Could not retrieve EPM sets."
+    }
+
+    #$setId = $null
+
+    # If setName is provided, search for it directly
+    if (-not [string]::IsNullOrEmpty($setName)) {
+        $selectedSet = $sets.Sets | Where-Object { $_.Name -eq $setName } | Select-Object -First 1
+
+        if ($selectedSet) {
+            return [PSCustomObject]@{
+                setId   = $selectedSet.Id
+                setName = $selectedSet.Name
+            }
+        } else {
+            Write-Log "Error: Set '$setName' not found in EPM." ERROR
+            throw "Invalid Set Name: $setName"
+        }
+    }
+
+    # If no setName is provided, prompt the user to select one
+    #Write-Log "No set name provided. Listing available sets for selection..." INFO
+
+    if ($sets.Sets.Count -eq 0) {
+        Write-Log "No sets available in EPM." ERROR
+        throw "No sets found. Cannot proceed."
+    }
+
+    Write-Box "Available Sets:" INFO
+
+    for ($i = 0; $i -lt $sets.Sets.Count; $i++) {
+        Write-Log "$($i + 1). $($sets.Sets[$i].Name)" INFO DarkCyan
+    }
+
+    # Prompt user for input with max retries
+    $maxRetries = 3
+    for ($attempt = 1; $attempt -le $maxRetries; $attempt++) {
+        $chosenSetNumber = Read-Host "Enter the number of the set you want to choose"
+
+        try {
+            $chosenSetNumber = [int]$chosenSetNumber
+
+            if ($chosenSetNumber -ge 1 -and $chosenSetNumber -le $sets.Sets.Count) {
+                $chosenSet = $sets.Sets[$chosenSetNumber - 1]
+                return [PSCustomObject]@{
+                    setId   = $chosenSet.Id
+                    setName = $chosenSet.Name
+                }
+            } else {
+                Write-Log "Invalid selection. Please enter a number between 1 and $($sets.Sets.Count)." ERROR
+            }
+        }
+        catch {
+            Write-Log "Invalid input. Please enter a valid number." ERROR
+        }
+    }
+
+    throw "Maximum attempts reached. Exiting set selection."
+}
+
+### Begin Script ###
+
+## Prepare log folder and file
+# Set default log folder if not provided
+if (-not $PSBoundParameters.ContainsKey('logFolder')) {
+    $scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Path
+    $logFolder = Join-Path $scriptDirectory "log"
+}
+
+# Ensure the log folder exists
+if (-not (Test-Path $logFolder)) {
+    New-Item -Path $logFolder -ItemType Directory -Force
+}
+
+# Create log file name based on timestamp and script name
+$timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
+$scriptName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
+$logFileName = "$timestamp`_$scriptName.log"
+$logFilePath = Join-Path $logFolder $logFileName
+##
+
+Write-Box "$scriptName"
+##
+
+# Request EPM Credentials
+$credential = Get-Credential -UserName $username -Message "Enter password for $username"
+
+# Authenticate
+$login = Connect-EPM -credential $credential -epmTenant $tenant
+
+# Create a session header with the authorization token
+$sessionHeader = @{
+    "Authorization" = "basic $($login.auth)"
+    "Content-Type" = "application/json"
+}
+
+# Get SetId
+$set = Get-EPMSetID -managerURL $($login.managerURL) -Headers $sessionHeader -setName $setName
+
+# Check the policyFile
+if (-not (Test-Path $policyFile)) {
+    Write-Log "The specified CSV file '$policyFile' was not found." ERROR
+    exit 1
+}
+
+# Import the CSV
+try {
+    $policyContent = Import-Csv -Path $policyFile -Header ComputerName,Policies
+}
+catch {
+    Write-Warning "Failed to import CSV file '$policyFile'. Please check its format."
+    Throw $_.Exception.Message
+}
+
+$csvComputerNames = $policyContent | Select-Object -ExpandProperty ComputerName
+
+#Get the policies list
+$policiesFilter = @{
+    "filter" = "PolicyType EQ ADV_WIN"
+}  | ConvertTo-Json
+
+$getPolicies = Invoke-EPMRestMethod -Uri "$($login.managerURL)/EPM/API/Sets/$($set.setId)/Policies/Server/Search" -Method 'POST' -Headers $sessionHeader -Body $policiesFilter
+
+# Store in hashtable for fster access
+$retrievedPoliciesMap = @{}
+
+foreach ($retrievedPolicy in $getPolicies.Policies) {
+    $retrievedPoliciesMap[$retrievedPolicy.PolicyName] = $retrievedPolicy.PolicyId
+}
+
+# Get the computer list
+$getComputerList = Invoke-EPMRestMethod -Uri "$($login.managerURL)/EPM/API/Sets/$($set.setId)/Computers" -Method 'GET' -Headers $sessionHeader
+
+foreach ($computer in $getComputerList.Computers) {
+    # Search the Computer Name in the CSV file
+    if ($csvComputerNames.Contains($computer.ComputerName)) {
+        Write-Log "Processing computer: $($computer.ComputerName)" INFO
+
+        $csvComputerName = $policyContent | Where-Object { $_.ComputerName -eq $computer.ComputerName }
+        $csvPoliciesName = $csvComputerName.Policies -split ';' | ForEach-Object { $_.Trim() }
+        foreach ($policy in $csvPoliciesName) {
+            Write-Log "Processing policy: $($policy)" INFO
+
+            $getPolicy = Invoke-EPMRestMethod -Uri "$($login.managerURL)/EPM/API/Sets/$($set.setId)/Policies/Server/$($retrievedPoliciesMap[$policy])" -Method 'GET' -Headers $sessionHeader
+            
+            # Forcing not applied to all computers
+            $getPolicy.Policy.IsAppliedToAllComputers=$False
+            
+            # Forcing policy Enabled
+            $getPolicy.Policy.IsActive=$True
+
+            ## Process the Executors
+            # Flag to determine if the computer was found in the existing executors
+            $computerFoundInExecutors = $false
+
+            # Iterate through existing executors to check if the computer name already exists
+            foreach ($executor in $getPolicy.Policy.Executors) {
+                if ($executor.Name -eq $computer.ComputerName) {
+                    
+                    $computerFoundInExecutors = $true
+                    break # Exit the inner foreach loop as soon as it's found
+                }
+            }
+
+            if ($computerFoundInExecutors) {
+                Write-Log "Computer '$($computer.ComputerName)' already exists in policy executors. Skipping policy update for this computer." WARN
+                # process other computers if there are any
+                continue 
+            } else {
+                Write-Log "Computer '$($computer.ComputerName)' not found in policy executors. Adding it." INFO
+
+                $newExecutor = [PSCustomObject]@{
+                    "Id"           = $computer.AgentId
+                    "Name"         = $computer.ComputerName
+                    "IsIncluded"   = $true
+                    "ExecutorType" = 1
+                }
+                ## Process the Executors Done
+
+                # Upload the policy
+                $getPolicy.Policy.Executors += $newExecutor
+                
+                $newPolicyJSON = $getPolicy.Policy | ConvertTo-Json -Depth 10
+                $updatePolicy = Invoke-EPMRestMethod -Uri "$($login.managerURL)/EPM/API/Sets/$($set.setId)/Policies/Server/$($retrievedPoliciesMap[$policy])" -Method 'PUT' -Headers $sessionHeader -Body $newPolicyJSON
+            }            
+        }
+    }
+}
+
