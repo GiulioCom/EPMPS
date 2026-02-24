0ae69417eead725c61c3bca800e4bc7275ccc665
Giulio Compagnone <giulio.compagnone@gmail.com>
2026-02-24 16:05:59 +0100

    Added New-AgentPolicy. Example script to create Agent Conf by reading a CSV of enpoints report and create an agent Conf having the SelfProtection disabled

---
+++ Policies/New-AgentPolicy.ps1
@@ -0,0 +1,525 @@
+<#
+.SYNOPSIS
+    Create new Agent policy by reading CSV file with he list computer
+    
+
+.DESCRIPTION
+    1. Get the agent configuration used as source.
+    2. Copy the configuration for new agent
+
+.PARAMETER username
+    The EPM username used for authentication (e.g., admin@cyberark.com).
+
+.PARAMETER setName
+    The specific EPM Set Name where the endpoints reside.
+
+.PARAMETER tenant
+    The EPM tenant/region prefix (e.g., 'eu', 'na', 'uk').
+
+.PARAMETER EndpointReportCSV
+    The full path to the CSV report containing the 'Agent Id' and 'Computer' columns.
+
+.NOTES
+    Author: Giulio Compagnone
+    Company: CyberArk
+    Version: 0.1 - POC
+    Date: 02/2026
+
+.EXAMPLE
+    1. .\New-AgentPolicy.ps1 -username "user@domain" -setName "MySet" -tenant "eu" -EndpointReportCSV "C:\Reports\AgentsList.csv"
+#>
+
+param (
+    [Parameter(Mandatory = $true, HelpMessage="Please enter valid EPM username (For example: user@domain)")]
+    [string]$username,
+
+    [Parameter(HelpMessage="Please enter valid EPM set name")]
+    [string]$setName = "",
+
+    [Parameter(Mandatory = $true, HelpMessage="Please enter valid EPM tenant (eu, uk, ....)")]
+    [ValidateSet("login", "eu", "uk", "au", "ca", "in", "jp", "sg", "it", "ch")]
+    [string]$tenant,
+
+    [Parameter(HelpMessage = "Enable logging to file and console")]
+    [switch]$log,
+
+    [Parameter(HelpMessage = "Specify the log file path")]
+    [string]$logFolder,
+
+    [Parameter(Mandatory = $true, HelpMessage = "Endpoints Report")]
+    [ValidateScript({
+        if (Test-Path $_ -PathType Leaf) {
+            $true
+        } else {
+            throw "File not found or is a directory: $_"
+        }
+    })]
+    [string]$EndpointReportCSV,
+
+    [Parameter(HelpMessage = "Specify the source agent policy name")]
+    [string]$sourceAgentPolicy
+
+
+)
+
+## Write-Host Wrapper and log management
+function Write-Log {
+    <#
+    .SYNOPSIS
+        Outputs a formatted log message to the console and a file.
+    #>
+    param (
+        [Parameter(Mandatory = $true)] [string]$message,
+        [Parameter(Mandatory = $true)] [ValidateSet("INFO", "WARN", "ERROR", "DEBUG")] [string]$severity,
+        [ConsoleColor]$ForegroundColor
+    )
+
+    if ($severity -eq "DEBUG" -and -not $ShowDebug) { return }
+
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $logMessage = "$timestamp [$($severity.PadRight(5))] $message"
+
+    if (-not $PSBoundParameters.ContainsKey('ForegroundColor')) {
+        $ForegroundColor = switch ($Severity) {
+            "INFO"  { "Green" }
+            "WARN"  { "Yellow" }
+            "ERROR" { "Red" }
+            "DEBUG" { "Gray" }
+        }
+    }
+
+    Write-Host $logMessage -ForegroundColor $ForegroundColor
+
+    if ($log) {
+        Add-Content -Path $LogPath -Value $logMessage
+    }
+}
+
+function Write-Box {
+    <#
+    .SYNOPSIS
+        Displays a centered title within a fixed 42-character decorative box.
+    #>
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({$_.Length -le 38})]
+        [string]$title
+    )
+
+    $totalWidth = 42
+    $contentWidth = $totalWidth - 2
+    
+    # Calculate padding for centering
+    $leftPadding  = [Math]::Floor(($contentWidth - $title.Length) / 2)
+    $rightPadding = $contentWidth - $title.Length - $leftPadding
+    
+    # Construct lines
+    $horizontalLine = "+" + ("-" * ($totalWidth - 2)) + "+"
+    $centeredText   = "|" + (" " * $leftPadding) + $title + (" " * $rightPadding) + "|"
+
+    $textProp = @{
+        "Severity"        = "INFO"
+        "ForegroundColor" = "Cyan"
+    }
+    
+    $textProp = @{
+        "Severity" = "INFO"
+        "ForegroundColor" = "Cyan"
+    }
+
+    Write-Log $horizontalLine @textProp
+    Write-Log $centeredText   @textProp
+    Write-Log $horizontalLine @textProp
+}
+
+## Invoke-RestMethod Wrapper
+function Invoke-EPMRestMethod {
+    <#
+    .SYNOPSIS
+        Invokes a REST API method with automatic retry logic in case of transient failures.
+
+    .DESCRIPTION
+        This function is designed to make REST API calls with automatic retries in case of specific errors, such as rate limiting.
+        It provides a robust way to handle transient failures and ensures that the API call is retried a specified number of times.
+
+    .PARAMETER URI
+        The Uniform Resource Identifier (URI) for the REST API endpoint.
+
+    .PARAMETER Method
+        The HTTP method (e.g., GET, POST, PUT, DELETE) for the API call.
+
+    .PARAMETER Body
+        The request body data to be sent in the API call (can be null for certain methods).
+
+    .PARAMETER Headers
+        Headers to include in the API request.
+    #>
+    param (
+        [string]$URI,
+        [string]$Method,
+        [object]$Body = $null,
+        [hashtable]$Headers = @{},
+        [int]$MaxRetries = 3,
+        [int]$RetryDelay = 120 # Default value, in case of the returned message doesn't contain the limit info
+    )
+
+    $retryCount = 0
+
+    while ($retryCount -lt $MaxRetries) {
+        try {
+            $response = Invoke-RestMethod -Uri $Uri -Method $Method -Body $Body -Headers $Headers -ErrorAction Stop
+            return $response
+        }
+        catch {
+            $ErrorDetailsMessage = $null
+
+            # Extract API error details if available
+            if ($_.ErrorDetails -and $_.ErrorDetails.Message) {
+                try {
+                    $ErrorDetailsMessage = $_.ErrorDetails.Message | ConvertFrom-Json -ErrorAction SilentlyContinue
+                }
+                catch {
+                    Write-Log "Failed to parse error message as JSON. Raw message: $($_.ErrorDetails.Message)" WARN
+                }
+            }
+
+            # Handle rate limit error (EPM00000AE)
+            if ($ErrorDetailsMessage -and $ErrorDetailsMessage.ErrorCode -eq "EPM00000AE") {
+                # Regex pattern to find numbers followed by "minute(s)"
+                $pattern = "\d+\s+minute"
+                $match = [regex]::Match($ErrorDetailsMessage.ErrorMessage, $pattern)
+                if ($match.Success) {
+                    $minutes = [int]($match.Value -replace '\s+minute', '')
+                    [int]$RetryDelay = $minutes * 60
+                    Write-Log "$($ErrorDetailsMessage.ErrorMessage) - Retrying in $RetryDelay seconds..." WARN
+                }
+
+                Write-Log "$($ErrorDetailsMessage.ErrorMessage) - Retrying in $RetryDelay seconds (default)..." WARN
+                Start-Sleep -Seconds $RetryDelay
+                $retryCount++
+            } else {
+                # Handle Body possible filter error 
+                if ($ErrorDetailsMessage.ErrorCode -eq "EPM000002E" -and $null -ne $Body) {
+                    Write-Log "API call failed at line $($MyInvocation.ScriptLineNumber) - ErrorCode: $($ErrorDetailsMessage.ErrorCode), ErrorMessage: $($ErrorDetailsMessage.ErrorMessage)" ERROR
+                    Write-Log "Please verify the filter body if present, as it could be the cause of this error code." ERROR
+                    throw "API call failed at line $($MyInvocation.ScriptLineNumber) - ErrorCode: $($ErrorDetailsMessage.ErrorCode), ErrorMessage: $($ErrorDetailsMessage.ErrorMessage)"
+                } else {
+                    # Log error only if it's NOT the handled EPM00000AE error
+                    Write-Log "API call failed at line $($MyInvocation.ScriptLineNumber) - ErrorCode: $($ErrorDetailsMessage.ErrorCode), ErrorMessage: $($ErrorDetailsMessage.ErrorMessage)" ERROR
+                    throw "API call failed at line $($MyInvocation.ScriptLineNumber) - ErrorCode: $($ErrorDetailsMessage.ErrorCode), ErrorMessage: $($ErrorDetailsMessage.ErrorMessage)"
+                }
+            }
+        }
+    }
+
+    # If all retries fail, log and throw an error
+    Write-Log "API call failed after $MaxRetries retries. URI: $URI" ERROR
+    throw "API call failed after $MaxRetries retries."
+}
+
+## EPM RestAPI Wrappers
+function Connect-EPM {
+    <#
+    .SYNOPSIS
+    Connects to the EPM (Endpoint Privilege Manager) using the provided credentials and tenant information.
+
+    .DESCRIPTION
+    This function performs authentication with the EPM API to obtain the manager URL and authentication details.
+
+    .PARAMETER credential
+    The credential object containing the username and password.
+
+    .PARAMETER epmTenant
+    The EPM tenant name.
+
+    .OUTPUTS
+    A custom object with the properties "managerURL" and "auth" representing the EPM connection information.
+
+    #>
+    param (
+        [Parameter(Mandatory = $true)]
+        [pscredential]$credential,  # Credential object containing the username and password
+
+        [Parameter(Mandatory = $true)]
+        [string]$epmTenant          # EPM tenant name
+    )
+
+    # Convert credential information to JSON for authentication
+    $authBody = @{
+        Username      = $credential.UserName
+        Password      = $credential.GetNetworkCredential().Password
+        ApplicationID = "Powershell"
+    } | ConvertTo-Json -Depth 3
+
+    $authHeaders = @{
+        "Content-Type" = "application/json"
+    }
+
+    try {
+        # Write-Log "Attempting to connect to EPM tenant: $epmTenant" INFO
+        $response = Invoke-EPMRestMethod -URI "https://$epmTenant.epm.cyberark.com/EPM/API/Auth/EPM/Logon" -Method 'POST' -Headers $authHeaders -Body $authBody
+
+        # Ensure the response contains the expected fields
+        if (-not $response -or -not $response.ManagerURL -or -not $response.EPMAuthenticationResult) {
+            throw "EPM authentication failed: Missing expected response fields."
+        }
+
+        # Write-Log "Successfully connected to EPM tenant: $epmTenant" INFO
+
+        # Return a custom object with connection information
+        return [PSCustomObject]@{
+            managerURL = $response.ManagerURL
+            auth       = $response.EPMAuthenticationResult
+        }
+    }
+    catch {
+        Write-Log "Failed to connect to EPM tenant: $epmTenant. Error: $_" ERROR
+        throw "Error connecting to EPM: $_"
+    }
+}
+
+function Get-EPMSetID {
+    <#
+    .SYNOPSIS
+    Retrieves the ID and name of an EPM set based on the provided parameters.
+
+    .DESCRIPTION
+    This function interacts with the EPM API to retrieve information about sets based on the specified parameters.
+
+    .PARAMETER managerURL
+    The URL of the EPM manager.
+
+    .PARAMETER Headers
+    The authorization headers.
+
+    .PARAMETER setName
+    The name of the EPM set to retrieve.
+
+    .OUTPUTS
+    A custom object with the properties "setId" and "setName" representing the EPM set information.
+    #>
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$managerURL,
+
+        [Parameter(Mandatory = $true)]
+        [hashtable]$Headers,
+
+        [string]$setName
+    )
+
+    # Retrieve list of sets
+    try {
+        #Write-Log "Retrieving EPM Sets from: $managerURL" INFO
+        $sets = Invoke-EPMRestMethod -URI "$managerURL/EPM/API/Sets" -Method 'GET' -Headers $Headers
+
+        if (-not $sets -or -not $sets.Sets) {
+            throw "No sets retrieved from EPM."
+        }
+    }
+    catch {
+        Write-Log "Failed to retrieve EPM Sets. Error: $_" ERROR
+        throw "Could not retrieve EPM sets."
+    }
+
+    #$setId = $null
+
+    # If setName is provided, search for it directly
+    if (-not [string]::IsNullOrEmpty($setName)) {
+        $selectedSet = $sets.Sets | Where-Object { $_.Name -eq $setName } | Select-Object -First 1
+
+        if ($selectedSet) {
+            return [PSCustomObject]@{
+                setId   = $selectedSet.Id
+                setName = $selectedSet.Name
+            }
+        } else {
+            Write-Log "Error: Set '$setName' not found in EPM." ERROR
+            throw "Invalid Set Name: $setName"
+        }
+    }
+
+    # If no setName is provided, prompt the user to select one
+    #Write-Log "No set name provided. Listing available sets for selection..." INFO
+
+    if ($sets.Sets.Count -eq 0) {
+        Write-Log "No sets available in EPM." ERROR
+        throw "No sets found. Cannot proceed."
+    }
+
+    Write-Box "Available Sets:"
+
+    for ($i = 0; $i -lt $sets.Sets.Count; $i++) {
+        Write-Log "$($i + 1). $($sets.Sets[$i].Name)" INFO DarkCyan
+    }
+
+    # Prompt user for input with max retries
+    $maxRetries = 3
+    for ($attempt = 1; $attempt -le $maxRetries; $attempt++) {
+        $chosenSetNumber = Read-Host "Enter the number of the set you want to choose"
+
+        try {
+            $chosenSetNumber = [int]$chosenSetNumber
+
+            if ($chosenSetNumber -ge 1 -and $chosenSetNumber -le $sets.Sets.Count) {
+                $chosenSet = $sets.Sets[$chosenSetNumber - 1]
+                return [PSCustomObject]@{
+                    setId   = $chosenSet.Id
+                    setName = $chosenSet.Name
+                }
+            } else {
+                Write-Log "Invalid selection. Please enter a number between 1 and $($sets.Sets.Count)." ERROR
+            }
+        }
+        catch {
+            Write-Log "Invalid input. Please enter a valid number." ERROR
+        }
+    }
+
+    throw "Maximum attempts reached. Exiting set selection."
+}
+
+<#
+function Get-ComputerID {
+    param (
+        [string] $compName
+    )
+
+    $getComputers = Invoke-EPMRestMethod -URI "$($login.managerURL)/EPM/API/Sets/$($set.setId)/Computers?`$filter=ComputerName eq '$compName'" -Method 'GET' -Headers $sessionHeader
+    
+    if ($getComputers.Computers.length -eq 0 ) {
+        Write-Log "No Computer named $compName was found, please refine your search." ERROR
+        exit 1
+    } elseif ($getComputers.Computers.length -gt 1) {
+        Write-Log "To many device found having name $compName was found, please refine your search." ERROR
+        exit 1
+    } else {
+        # Write-Log "$compName having ID: $($getComputers.Computers[0].AgentId)" INFO
+        return $getComputers.Computers[0].AgentId
+    }
+}
+#>
+
+# Logging setup
+$scriptName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
+if ($log) {
+    if (-not $PSBoundParameters.ContainsKey('logFolder')) {
+        $scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Path
+        $logFolder = Join-Path $scriptDirectory "log"
+    }
+
+    # Ensure the log folder exists
+    if (-not (Test-Path $logFolder)) {
+        New-Item -Path $logFolder -ItemType Directory -Force
+    }
+
+    # Create log file name based on timestamp and script name
+    $timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
+    $logFileName = "$timestamp`_$scriptName.log"
+    $logFilePath = Join-Path $logFolder $logFileName
+
+    Write-Log "Logging enabled. Log file: $logFilePath" INFO
+}
+## Log file done
+
+Write-Box "$scriptName"
+
+# Validate CSV
+$RawHeader = (Get-Content -Path $EndpointReportCSV -TotalCount 1).Split(',').Trim()
+$RequiredHeaders = @('Agent Id', 'Computer')
+
+foreach ($Header in $RequiredHeaders) {
+    if ($Header -notin $RawHeader) {
+        $ErrorMessage = "Schema Error: Missing required column '$Header' in $EndpointReportCSV"
+        Write-Log $ErrorMessage ERROR
+        throw $ErrorMessage
+    }
+}
+
+# Request EPM Credentials
+$credential = Get-Credential -UserName $username -Message "Enter password for $username"
+
+# Authenticate
+$login = Connect-EPM -credential $credential -epmTenant $tenant
+
+# Create a session header with the authorization token
+$sessionHeader = @{
+    "Authorization" = "basic $($login.auth)"
+}
+
+# Get SetId
+$set = Get-EPMSetID -managerURL $($login.managerURL) -Headers $sessionHeader -setName $setName
+Write-Box "$($set.setName)"
+
+# General URI
+$URI = "$($login.managerURL)/EPM/API/Sets/$($set.setId)"
+
+# Get the agent configuration policy 
+$agentConfId = $null
+$agentConfDetails = $null
+
+if ($PSBoundParameters.ContainsKey('sourceAgentPolicy')) {
+    
+    $agentPolicies = Invoke-EPMRestMethod -URI "$URI/Policies/AgentConfiguration/Search" -Method 'POST' -Headers $sessionHeader
+    $targetPolicy = $agentPolicies.Policies.Where({$_.PolicyName -eq $sourceAgentPolicy}, 'First')
+
+    if (-not $targetPolicy) {
+        Write-Log "Source policy '$sourceAgentPolicy' not found on server." ERROR
+        return
+    }
+
+    $agentConfId = $targetPolicy.PolicyId
+    Write-Log "Found source policy: $sourceAgentPolicy (ID: $agentConfId)" INFO
+} else {
+    Write-Log "Parameter sourceAgentPolicy not specified, using the default policy as source policy." WARN
+    $agentConfId = $set.setId
+}
+
+Write-Log "Loading the Agent Configuration" INFO
+$agentConfDetails = Invoke-EPMRestMethod -URI "$URI/Policies/AgentConfiguration/$agentConfId" -Method 'GET' -Headers $sessionHeader
+
+Write-Log "Importing data from $EndpointReportCSV..." INFO
+Import-Csv -Path $EndpointReportCSV | ForEach-Object {    
+
+    $DeviceName = $_.Computer
+    $AgentId = $_.'Agent Id'
+    
+    if ([string]::IsNullOrWhiteSpace($DeviceName) -or -not ([guid]::TryParse($AgentId, [ref][guid]::Empty))) {
+        Write-Log "Skipping row Computer name or AgentId is empty: $(($_.psobject.Properties.Value) -join ',')." WARN
+        continue 
+    }
+    
+    $newAgentConf = $agentConfDetails.policy | ConvertTo-Json -Depth 10 | ConvertFrom-Json
+    
+    # Remove the ID from the conf policy 
+    $newAgentConf.PSObject.Properties.Remove("Id")
+
+    # Update Agent ID and Computer Name from the conf policy
+    $newAgentConf.Executors = @(
+        [PSCustomObject]@{
+            id = $AgentId
+            Name = $DeviceName
+            ExecutorType = 1
+            IsIncluded = $true
+        }
+    )
+    
+    # Update IsAppliedToAllComputers
+    $newAgentConf.IsAppliedToAllComputers = $false
+  
+    # Update Policy Name from the conf policy
+    $newAgentConf.Name = $DeviceName
+
+    # Disable Agent Self Defense
+    $newAgentConf.ExtendedProtection.AgentSelfDefense.Value = $false
+
+    Write-Log "Uploading Agent Configuration for $DeviceName..." INFO
+    $updateAgentConfParam = @{
+        URI = "$URI/Policies/AgentConfiguration"
+        Method = 'POST'
+        Headers = $sessionHeader
+        Body = $newAgentConf | ConvertTo-Json -Compress -Depth 10
+    }
+    
+    $null = Invoke-EPMRestMethod @updateAgentConfParam
+}
