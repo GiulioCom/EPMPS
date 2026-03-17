5d362b590506a5c15d48358f50460c8d44fdb4f3
Giulio Compagnone <giulio.compagnone@gmail.com>
2026-03-17 20:02:08 +0100

    Improve detection of duplicated New Agent ID. Duplicated New Agent ID won't be deleted

--- Endpoints/EPMDeleteDuplicateEndpointsFromFile.ps1
+++ Endpoints/EPMDeleteDuplicateEndpointsFromFile.ps1
@@ -137,27 +137,27 @@ function Write-Box {
 
 ## Invoke-RestMethod Wrapper
 function Invoke-EPMRestMethod {
-    <#
-    .SYNOPSIS
-        Invokes a REST API method with automatic retry logic in case of transient failures.
+<#
+.SYNOPSIS
+    Invokes a REST API method with automatic retry logic in case of transient failures.
 
-    .DESCRIPTION
-        This function is designed to make REST API calls with automatic retries in case of specific errors, such as rate limiting.
-        It provides a robust way to handle transient failures and ensures that the API call is retried a specified number of times.
+.DESCRIPTION
+    This function is designed to make REST API calls with automatic retries in case of specific errors, such as rate limiting.
+    It provides a robust way to handle transient failures and ensures that the API call is retried a specified number of times.
 
-    .PARAMETER URI
-        The Uniform Resource Identifier (URI) for the REST API endpoint.
+.PARAMETER URI
+    The Uniform Resource Identifier (URI) for the REST API endpoint.
 
-    .PARAMETER Method
-        The HTTP method (e.g., GET, POST, PUT, DELETE) for the API call.
+.PARAMETER Method
+    The HTTP method (e.g., GET, POST, PUT, DELETE) for the API call.
 
-    .PARAMETER Body
-        The request body data to be sent in the API call (can be null for certain methods).
+.PARAMETER Body
+    The request body data to be sent in the API call (can be null for certain methods).
 
-    .PARAMETER Headers
-        Headers to include in the API request.
-    #>
-    param (
+.PARAMETER Headers
+    Headers to include in the API request.
+#>
+param (
         [string]$URI,
         [string]$Method,
         [object]$Body = $null,
@@ -195,19 +195,25 @@ function Invoke-EPMRestMethod {
                     $minutes = [int]($match.Value -replace '\s+minute', '')
                     [int]$RetryDelay = $minutes * 60
                     Write-Log "$($ErrorDetailsMessage.ErrorMessage) - Retrying in $RetryDelay seconds..." WARN
+                } else {
+                    Write-Log "$($ErrorDetailsMessage.ErrorMessage) - Retrying in $RetryDelay seconds (default)..." WARN
                 }
-
-                Write-Log "$($ErrorDetailsMessage.ErrorMessage) - Retrying in $RetryDelay seconds (default)..." WARN
                 Start-Sleep -Seconds $RetryDelay
                 $retryCount++
             } else {
-                # Handle Body possible filter error 
+                
                 if ($ErrorDetailsMessage.ErrorCode -eq "EPM000002E" -and $null -ne $Body) {
+                # Handle Body possible filter error 
                     Write-Log "API call failed at line $($MyInvocation.ScriptLineNumber) - ErrorCode: $($ErrorDetailsMessage.ErrorCode), ErrorMessage: $($ErrorDetailsMessage.ErrorMessage)" ERROR
                     Write-Log "Please verify the filter body if present, as it could be the cause of this error code." ERROR
                     throw "API call failed at line $($MyInvocation.ScriptLineNumber) - ErrorCode: $($ErrorDetailsMessage.ErrorCode), ErrorMessage: $($ErrorDetailsMessage.ErrorMessage)"
-                } else {
-                    # Log error only if it's NOT the handled EPM00000AE error
+                } elseif ($ErrorDetailsMessage.ErrorCode -eq "EPM000012E") {
+                # Handle Error EPM000012E - "ErrorMessage: EPM cannot identify the following target computers that were previously selected."
+                    Write-Log "API call failed at line $($MyInvocation.ScriptLineNumber) - ErrorCode: $($ErrorDetailsMessage.ErrorCode), ErrorMessage: $($ErrorDetailsMessage.ErrorMessage)" ERROR
+                    return
+                }
+                else {
+                # Log any other error
                     Write-Log "API call failed at line $($MyInvocation.ScriptLineNumber) - ErrorCode: $($ErrorDetailsMessage.ErrorCode), ErrorMessage: $($ErrorDetailsMessage.ErrorMessage)" ERROR
                     throw "API call failed at line $($MyInvocation.ScriptLineNumber) - ErrorCode: $($ErrorDetailsMessage.ErrorCode), ErrorMessage: $($ErrorDetailsMessage.ErrorMessage)"
                 }
@@ -222,23 +228,23 @@ function Invoke-EPMRestMethod {
 
 ## EPM RestAPI Wrappers
 function Connect-EPM {
-    <#
-    .SYNOPSIS
-    Connects to the EPM (Endpoint Privilege Manager) using the provided credentials and tenant information.
+<#
+.SYNOPSIS
+Connects to the EPM (Endpoint Privilege Manager) using the provided credentials and tenant information.
 
-    .DESCRIPTION
-    This function performs authentication with the EPM API to obtain the manager URL and authentication details.
+.DESCRIPTION
+This function performs authentication with the EPM API to obtain the manager URL and authentication details.
 
-    .PARAMETER credential
-    The credential object containing the username and password.
+.PARAMETER credential
+The credential object containing the username and password.
 
-    .PARAMETER epmTenant
-    The EPM tenant name.
+.PARAMETER epmTenant
+The EPM tenant name.
 
-    .OUTPUTS
-    A custom object with the properties "managerURL" and "auth" representing the EPM connection information.
+.OUTPUTS
+A custom object with the properties "managerURL" and "auth" representing the EPM connection information.
 
-    #>
+#>
     param (
         [Parameter(Mandatory = $true)]
         [pscredential]$credential,  # Credential object containing the username and password
@@ -282,25 +288,25 @@ function Connect-EPM {
 }
 
 function Get-EPMSetID {
-    <#
-    .SYNOPSIS
-    Retrieves the ID and name of an EPM set based on the provided parameters.
+<#
+.SYNOPSIS
+Retrieves the ID and name of an EPM set based on the provided parameters.
 
-    .DESCRIPTION
-    This function interacts with the EPM API to retrieve information about sets based on the specified parameters.
+.DESCRIPTION
+This function interacts with the EPM API to retrieve information about sets based on the specified parameters.
 
-    .PARAMETER managerURL
-    The URL of the EPM manager.
+.PARAMETER managerURL
+The URL of the EPM manager.
 
-    .PARAMETER Headers
-    The authorization headers.
+.PARAMETER Headers
+The authorization headers.
 
-    .PARAMETER setName
-    The name of the EPM set to retrieve.
+.PARAMETER setName
+The name of the EPM set to retrieve.
 
-    .OUTPUTS
-    A custom object with the properties "setId" and "setName" representing the EPM set information.
-    #>
+.OUTPUTS
+A custom object with the properties "setId" and "setName" representing the EPM set information.
+#>
     param (
         [Parameter(Mandatory = $true)]
         [string]$managerURL,
@@ -325,8 +331,6 @@ function Get-EPMSetID {
         throw "Could not retrieve EPM sets."
     }
 
-    #$setId = $null
-
     # If setName is provided, search for it directly
     if (-not [string]::IsNullOrEmpty($setName)) {
         $selectedSet = $sets.Sets | Where-Object { $_.Name -eq $setName } | Select-Object -First 1
@@ -341,9 +345,6 @@ function Get-EPMSetID {
             throw "Invalid Set Name: $setName"
         }
     }
-
-    # If no setName is provided, prompt the user to select one
-    #Write-Log "No set name provided. Listing available sets for selection..." INFO
 
     if ($sets.Sets.Count -eq 0) {
         Write-Log "No sets available in EPM." ERROR
@@ -384,23 +385,28 @@ function Get-EPMSetID {
 
 ### Begin Script ###
 
+$scriptName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
+
 ## Prepare log folder and file
 # Set default log folder if not provided
-if (-not $PSBoundParameters.ContainsKey('logFolder')) {
-    $scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Path
-    $logFolder = Join-Path $scriptDirectory "log"
-}
+if ($log) {
+    if (-not $PSBoundParameters.ContainsKey('logFolder')) {
+        $scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Path
+        $logFolder = Join-Path $scriptDirectory "log"
+    }
 
-# Ensure the log folder exists
-if (-not (Test-Path $logFolder)) {
-    New-Item -Path $logFolder -ItemType Directory -Force
-}
+    # Ensure the log folder exists
+    if (-not (Test-Path $logFolder)) {
+        New-Item -Path $logFolder -ItemType Directory -Force
+    }
 
-# Create log file name based on timestamp and script name
-$timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
-$scriptName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
-$logFileName = "$timestamp`_$scriptName.log"
-$logFilePath = Join-Path $logFolder $logFileName
+    # Create log file name based on timestamp and script name
+    $timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
+    $logFileName = "$timestamp`_$scriptName.log"
+    $logFilePath = Join-Path $logFolder $logFileName
+
+    Write-Log "Logging enabled. Log file: $logFilePath" INFO
+}
 ##
 
 Write-Box "$scriptName"
@@ -410,17 +416,68 @@ if (-not $delete){ Write-Log "Analysis Mode Enabled." INFO DarkGreen}
 else { Write-Log "Delete Mode Enabled." INFO DarkGreen}
 
 Write-Log "Importing data from $EndpointReportCSV..." INFO
-$EndpointstotalCount = (Get-Content $EndpointReportCSV | Measure-Object).Count - 1
 
-Write-Log "Searching for duplicated..." INFO
+# Validate CSV
+$FirstRow = Import-Csv -Path $EndpointReportCSV | Select-Object -First 1
+if (-not $FirstRow) {
+    $ErrorMessage = "Schema Error: $EndpointReportCSV is empty or missing valid headers."
+    Write-Log $ErrorMessage ERROR
+    throw $ErrorMessage
+}
 
-$DuplicatedEndpoints = [System.Collections.Generic.List[Object]]::new()
-$LatestEndpoints = @{} # Key = Computer Name (Endpoint.name), Value = Endpoint Object
+$RawHeader = $FirstRow.psobject.properties.name
+$RequiredHeaders = @('Agent Id', 'Computer', 'New Agent Id', 'Agent Version')
 
+foreach ($Header in $RequiredHeaders) {
+    if ($Header -notin $RawHeader) {
+        $ErrorMessage = "Schema Error: Missing required column '$Header' in $EndpointReportCSV"
+        Write-Log $ErrorMessage ERROR
+        throw $ErrorMessage
+    }
+}
+
+# Calculate total rows
+$EndpointsTotalCount = 0
+$StreamReader = [System.IO.File]::OpenText($EndpointReportCSV)
+try {
+    while ($null -ne $StreamReader.ReadLine()) {
+        $EndpointsTotalCount++
+    }
+}
+finally {
+    $StreamReader.Dispose()
+}
+$EndpointsTotalCount = [Math]::Max(0, $EndpointsTotalCount - 1)
+
+Write-Log "Searching Duplicated by Agent ID..." INFO
+
+$NewAgentIdFrequency = [System.Collections.Generic.Dictionary[string, int]]::new()
+Import-Csv -Path $EndpointReportCSV | ForEach-Object {
+    $NewAgentId = $_."New Agent Id"
+
+    if (-not [string]::IsNullOrWhiteSpace($NewAgentId) -or $NewAgentId -eq "00000000-0000-0000-0000-000000000000") {
+        if ($NewAgentIdFrequency.ContainsKey($AgentId)) {
+            $NewAgentIdFrequency[$NewAgentId]++
+        } else {
+            $NewAgentIdFrequency[$NewAgentId] = 1
+        }
+    }
+}
+
+Write-Log "Searching for duplicated by Computer Name..." INFO
+
+# Define the Progress Bar
 $processedEndpoints = 0
 $updateInterval = [Math]::Max(1, [Math]::Floor($EndpointsTotalCount / 100)) # Update every 1%
 
+# Duplicates Counters
+$DuplicatedEndpoints = [System.Collections.Generic.List[PSCustomObject]]::new()
+$DuplicatedNewAgentIds  = [System.Collections.Generic.List[PSCustomObject]]::new()
+$LatestEndpoints = @{} # Key = Computer Name (Endpoint.name), Value = Endpoint Object
+
 Import-Csv -Path $EndpointReportCSV | ForEach-Object {    
+
+    $processedEndpoints++
 
     if ($processedEndpoints % $updateInterval -eq 0) {
         $Percent = (($processedEndpoints / $EndpointstotalCount) * 100)
@@ -428,25 +485,36 @@ Import-Csv -Path $EndpointReportCSV | ForEach-Object {
     }
     
     $LastSeenDate = $_."Last Seen" -as [DateTime]
-    if (-not $LastSeenDate) { $LastSeenDate = [DateTime]::MinValue }
+    if (-not $LastSeenDate) {
+        Write-Log "'Last Seen' not valid is empty for Computer '$($_.Computer) - NewID: $($_."New Agent Id") (LastSeen: $($_."Last Seen"), Version: $($_."Agent Version"))'." ERROR
+        return
+    }
     
     $CurrentEndpoint = [PSCustomObject]@{
         Computer     = $_.Computer
         NewAgentId   = $_."New Agent Id"
         AgentVersion = $_."Agent Version"
-        _ParsedDate  = $LastSeenDate
+        LastSeen     = $LastSeenDate
     }
 
-    if ($LatestEndpoints.ContainsKey($CurrentEndpoint.Computer)) {
-        $ExistingEndpoint = $LatestEndpoints[$CurrentEndpoint.Computer]
-        
-        if ($CurrentEndpoint._ParsedDate -gt $ExistingEndpoint._ParsedDate) {
-            $DuplicatedEndpoints.Add($ExistingEndpoint)
+    if (-not [string]::IsNullOrWhiteSpace($_."New Agent Id") -and $NewAgentIdFrequency[$_."New Agent Id"] -gt 1) {
+        $DuplicatedNewAgentIds.Add($CurrentEndpoint)
+    } else {
+
+        if ($LatestEndpoints.ContainsKey($CurrentEndpoint.Computer)) {
+            $ExistingEndpoint = $LatestEndpoints[$CurrentEndpoint.Computer]
+            
+            if ($CurrentEndpoint.LastSeen -gt $ExistingEndpoint.LastSeen) {
+                $DuplicatedEndpoints.Add($ExistingEndpoint)
+                $LatestEndpoints[$CurrentEndpoint.Computer] = $CurrentEndpoint
+            } else {
+                $DuplicatedEndpoints.Add($CurrentEndpoint)
+            }
+        } else {
             $LatestEndpoints[$CurrentEndpoint.Computer] = $CurrentEndpoint
-        } else { $DuplicatedEndpoints.Add($CurrentEndpoint) }
+        }
     }
-    else { $LatestEndpoints[$CurrentEndpoint.Computer] = $CurrentEndpoint }
-    $processedEndpoints++
+    
 }
 
 Write-Progress -Activity "Processing Endpoints $($EndpointsTotalCount)" -Status "Completed: $processedEndpoints Endpoints" -PercentComplete 100 -Completed
@@ -454,7 +522,7 @@ Write-Progress -Activity "Processing Endpoints $($EndpointsTotalCount)" -Status 
 Write-Log "Identified $($DuplicatedEndpoints.Count) duplicated endpoints to remove." INFO
 
 $IdList = foreach ($Dup in $DuplicatedEndpoints) {
-    $EndpointInfoMessage = "$($Dup.Computer) - ID: $($Dup.NewAgentId) (LastSeen: $($Dup._ParsedDate), Version: $($Dup.AgentVersion))"
+    $EndpointInfoMessage = "$($Dup.Computer) - NewID: $($Dup.NewAgentId) (LastSeen: $($Dup.LastSeen), Version: $($Dup.AgentVersion))"
 
     if ($Dup.NewAgentId -eq "00000000-0000-0000-0000-000000000000") {
         Write-Log "Agent not compatible with script. Use MyComputer instead: $EndpointInfoMessage" WARN
@@ -500,14 +568,16 @@ if ($delete){
             $FilterString = "id IN " + ($Batch -join ",")
 
             if ($FilterString.Length -gt 10000) {
-                Write-Log "FATAL ERROR: Calculated filter string length ($($FilterString.Length)) exceeded 10000 chars. ABORTING BATCH." ERROR
+                Write-Log "Filter string length $($FilterString.Length) exceeded 10.000 chars. Aborting." ERROR
                 continue
             }
     
-            #Write-Log "Processing batch starting with ID $($Batch[0]) (Count: $($Batch.Count), Length: $($FilterString.Length))." INFO
-    
-            $DeleteBody = @{ "filter" = $FilterString }
-            if ($ForceDelete) { $DeleteBody.force = $true }
+            $DeleteBody = @{
+                "filter" = $FilterString
+            }
+            if ($ForceDelete) {
+                $DeleteBody.force = $true
+            }
             $DeleteBody = $DeleteBody | ConvertTo-Json
 
             $Result = Invoke-EPMRestMethod -Uri "$($login.managerURL)/EPM/API/Sets/$($set.setId)/Endpoints/delete" -Method 'POST' -Headers $sessionHeader -Body $DeleteBody
@@ -522,5 +592,9 @@ if ($delete){
                 }
             } else { Write-Log "Not Deleted: $($Batch.Count) - Status: ID not presente or valid." WARN }
         }
-    } else { Write-Log "No Duplicated Endpoints" WARN }
-} else { Write-Log "Demo Mode - No deletion" WARN }
+    } else {
+        Write-Log "No Duplicated Endpoints" WARN
+    }
+} else {
+    Write-Log "Demo Mode - No deletion" WARN
+}
